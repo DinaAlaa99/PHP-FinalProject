e153dcb3ce268bb8090e7ba124c8c269d3d93342
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-03-11 14:03:02 +0200

    handling some bugs

--- index.php
+++ index.php
@@ -1,54 +1,49 @@
 <?php
-require_once ("vendor/autoload.php");
+require_once "vendor/autoload.php";
 $mydb = new dbconnection();
 //$mydb->insert_user();
 
-require_once ("View/login.php");
-
 //$_SESSION["id"]=5;
 
+//$page="paymentPage";
 
-//$current_url = "View/";
-
-
-
-$check=login::check_Login();
+$check = login::check_Login();
 
 // if there's cookie
-if($check)
-{
-    echo"echeck remeberme";
-    header("Location: View/download.php");
+if ($check) {
+    echo "echeck remeberme";
+    $page = "download";
+
+} else {
+    //header("location:index.php?page=paymentPage");
+    //require_once "View/paymentPage.php";
+    //echo "jhhbhb";
+    $page = "paymentPage";
+
+    if (validate::validate_data()) {
+        //require_once "View/login.php";
+        $page = "login";
+    }
+
+    //echo"false index";
 }
-else{
-   // header("Location:View/paymentPage.php");
-    //die();
-    echo "jhhbhb";
-    require_once "View/paymentPage.php";
-    // login::redirect($current_url.'paymentPage.php');
-   //header("location:http://localhost/PHP-FINALPROJECT/finalProject/View/paymentPage.php");
-   //exit();
- 
-
-   if( validate::validate_data())
-   {
-   require_once "View/login.php";}
-   else 
-   require_once "View/unsuccessful.php";
-
 
 if (isset($_POST["login"])) {
-    $email=$_POST["email"];
-    $password=$_POST["password"];
-    $user= new user($email,$password);
-    $check=dbconnection::select_user($user);
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $user = new user($email, $password);
+    $check = dbconnection::select_user($user);
+    if ($check) {
+        $page = "download";
+    }
 
- //  dbconnection::insert_user($user);
+    //  dbconnection::insert_user($user);
     //var_dump($user);
 
-    echo"<br>";
+    echo "<br>";
     //echo $check;
-    echo"<br>";
+    echo "<br>";
     /*dbconnection:: insert_token($user);*/
+
 }
-}
+require_once "View/$page.php";
